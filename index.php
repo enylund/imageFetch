a91751e468d683e6ffed23d64c4930ea0daca165
enylund <nylund.eric@gmail.com>
2018-05-18 11:42:21 -0400

    starts the homepage design

--- index.php
+++ index.php
@@ -1,58 +1,54 @@
 <?php
-  include_once 'simple_html_dom.php';
 
-    fetchImages("https://www.att.com/","cnn");
-
-    function fetchImages($url, $directory) {
-
-      $html           = file_get_html($url);
-      $imageDirectory = "img/".$directory;
-      $date           = date("Y-m-d");
-      $dateDirectory  = $imageDirectory."/".$date;
-      $regexImageName = '/[\w\.\-\$]+(?=png|jpg|gif)\w+/';
+  function fetchPath() {
+    $dirs = array_filter(glob('img/*'), 'is_dir');
+    $rand_key = array_rand($dirs);
+    $rand_folder = $dirs[$rand_key];
 
 
-      if ( !file_exists($imageDirectory) ) {
-        mkdir ($imageDirectory);
-      }
+    $subdirs = array_filter(glob($rand_folder.'/*'), 'is_dir');
+    $rand_sub_key = array_rand($subdirs);
+    $rand_sub_folder = $subdirs[$rand_sub_key];
 
-      if ( !file_exists($dateDirectory) ) {
-        mkdir ($dateDirectory);
-      }
+    $rand_image_array = scandir($rand_sub_folder);
 
+    $rand_image_key = array_rand($rand_image_array);
+    $rand_image = $rand_image_array[$rand_image_key];
 
-      // Start contact sheet head
-      $dir = $dateDirectory.'/cache';
-      if ( !file_exists($dir) ) {
-        mkdir ($dir, 0744);
-      }
-      $cachefile = $dir.'/'.$date.'.html';
-      ob_start(); // start the output buffer
-      // End Cache Head
+    return $rand_sub_folder.'/'.$rand_image;
+  }
 
-      // Find all images
-      foreach($html->find('img') as $img) {
-        $property = 'data-src';
-        preg_match_all($regexImageName, $img->$property, $matches, PREG_SET_ORDER, 0);
+  $final_path = fetchPath();
+  $regexImageName = '/[\w\.\-\$]+(?=png|jpg|gif)\w+/';
+  preg_match_all($regexImageName, $final_path, $matches, PREG_SET_ORDER, 0);
 
-        $filename = $dateDirectory."/".$matches[0][0];
+  while (empty($matches)) {
+    $final_path = fetchPath();
+    preg_match_all($regexImageName, $final_path, $matches, PREG_SET_ORDER, 0);
+  }
 
-        if ( !file_exists($filename) ) {
-            //Get the file
-            echo('<img src="'.$property.'">');
-            $content = file_get_contents($property);
-            //Store in the filesystem.
-            $fp = fopen($filename, "w");
-            fwrite($fp, $content);
-            fclose($fp);
-        }
-      }
-
-      // Start Cache Footer
-      $fp = fopen($cachefile, 'a'); // open the cache file for writing
-      fwrite($fp, ob_get_contents()); // save the contents of output buffer to the file
-      fclose($fp); // close the file
-      ob_end_flush(); // Send the output to the browser
-      // End Cache Footer
-    }
+  print_r($final_path);
 ?>
+
+<!DOCTYPE html>
+<!--[if lte IE 6]><html class="preIE7 preIE8 preIE9"><![endif]-->
+<!--[if IE 7]><html class="preIE8 preIE9"><![endif]-->
+<!--[if IE 8]><html class="preIE9"><![endif]-->
+<!--[if gte IE 9]><!-->
+<html style="background: url(<?php echo $final_path ?>) no-repeat center center fixed;background-size: cover; background-color: red;
+  background-blend-mode: multiply;"><!--<![endif]-->
+  <head>
+    <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Image Fetch</title>
+  <meta name="author" content="name">
+  <meta name="description" content="description here">
+  <meta name="keywords" content="keywords,here">
+  <link rel="stylesheet" href="assets/main.css" type="text/css">
+  </head>
+  <body>
+    <div class="background-image"></div>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+  </body>
+</html>
