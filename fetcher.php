32984e03538d1bbaebb98da7013f1be30c19974e
enylund <nylund.eric@gmail.com>
2018-05-18 15:15:11 -0400

    adds a fetcher file that gets all images

---
+++ fetcher.php
@@ -0,0 +1,90 @@
+<?php
+  $curl = curl_init("http://localhost:8888/image-fetch/africanews.php");
+  curl_exec($curl);
+  echo "Finished africanews.php <br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/aljazeera.php");
+  curl_exec($curl);
+  echo "Finished aljazeera.php <br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/artforum.php");
+  curl_exec($curl);
+  echo "Finished artforum.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/breitbart.php");
+  curl_exec($curl);
+  echo "Finished breitbart.php <br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/cryptonews.php");
+  curl_exec($curl);
+  echo "Finished cryptonews.php <br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/espn.php");
+  curl_exec($curl);
+  echo "Finished espn.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/foxnews.php");
+  curl_exec($curl);
+  echo "Finished foxnews.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/gamespot.php");
+  curl_exec($curl);
+  echo "Finished gamespot.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/gunmagwarehouse.php");
+  curl_exec($curl);
+  echo "Finished gunmagwarehouse.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/hiphopdx.php");
+  curl_exec($curl);
+  echo "Finished hiphopdx.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/infowars.php");
+  curl_exec($curl);
+  echo "Finished infowars.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/joblo.php");
+  curl_exec($curl);
+  echo "Finished joblo.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/aljazeera.php");
+  curl_exec($curl);
+  echo "Finished africanews <br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/nationalenquirer.php");
+  curl_exec($curl);
+  echo "Finished nationalenquirer.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/ny-post.php");
+  curl_exec($curl);
+  echo "Finished ny-post.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/ny-times.php");
+  curl_exec($curl);
+  echo "Finished ny-times.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/thesource.php");
+  curl_exec($curl);
+  echo "Finished thesource.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/theverge.php");
+  curl_exec($curl);
+  echo "Finished theverge.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/time.php");
+  curl_exec($curl);
+  echo "Finished time.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/tmz.php");
+  curl_exec($curl);
+  echo "Finished tmz.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/washingtontimes.php");
+  curl_exec($curl);
+  echo "Finished washingtontimes.php<br>";
+
+  $curl = curl_init("http://localhost:8888/image-fetch/wired.php");
+  curl_exec($curl);
+  echo "Finished wired.php<br>";
+
+?>
