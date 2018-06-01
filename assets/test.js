d28e138eb5e856c78b9958085c204bc59f829f5b
enylund <nylund.eric@gmail.com>
2018-06-01 08:56:00 -0400

    push test.php

--- assets/test.js
+++ assets/test.js
@@ -1,62 +1,69 @@
-var img = new Image();
+// Global variables
+var speed = 40,
+x = 0,
+imgW,
+imgH,
+clearX,
+clearY,
+ctx,
+srcArray,
+CanvasXSize,
+CanvasYSize,
+init = function (callback) {
+    var srcArray = ['img/hiphopdx/2018-05-18/nick-grant.jpg', 'img/aljazeera/2018-05-22/f1398e29dd204095a73c1829c4daf866_6.png'];
 
-// User Variables - customize these to change the image being scrolled, its
-// direction, and the speed.
-
-img.src = 'img/hiphopdx/2018-05-18/nick-grant.jpg';
-var CanvasXSize = img.naturalWidth;
-var CanvasYSize = img.naturalHeight;
-var speed = 20; // lower is faster
-
-// Main program
-
-var dx = 0.75;
-var imgW;
-var imgH;
-var x = 0;
-var clearX;
-var clearY;
-var ctx;
-
-img.onload = function() {
-    imgW = img.width;
-    imgH = img.height;
-
-    clearX = CanvasXSize;
-    clearY = CanvasYSize;
-
-    // get canvas context
-    canvas = document.getElementById('image-canvas')
-    ctx = canvas.getContext('2d');
-    canvas.width = imgW;
-    canvas.height = imgH;
-
-    // set refresh rate
-    return setInterval(draw, speed);
-}
-
-function draw() {
+    img          = new Image();
+    img.src      = srcArray[Math.floor(Math.random()*srcArray.length)];;
+    CanvasXSize  = img.naturalWidth;
+    CanvasYSize  = img.naturalHeight;
+    x            = 0;
+    callback(img);
+},
+draw = function draw() {
     ctx.clearRect(0, 0, clearX, clearY); // clear the canvas
 
     // draw image
     ctx.drawImage(img, 0, 0);
     img.style.display = 'none';
-    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    var data = imageData.data;
+    var imageData     = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data          = imageData.data;
 
-
-    for (var i = 0; i < data.length; i += 4) {
-      if (i > x){
-        data[i + 3] = 0;
-      } else if( data[i] > 100 && data[i + 1] > 100  & data[i + 2] > 100 ){
-        data[i + 3] = 0;
-      } else {
-        data[i]     = 0; // red
-        data[i + 1] = 0; // green
-        data[i + 2] = 0; // blue
-      }
+    if (x < data.length - 10000) {
+        for (var i = 0; i < data.length; i += 4) {
+          if (i > x){
+            data[i + 3] = 0;
+          } else if( data[i] > 100 && data[i + 1] > 100  & data[i + 2] > 100 ){
+            data[i + 3] = 0;
+          } else {
+            data[i]     = 0; // red
+            data[i + 1] = 0; // green
+            data[i + 2] = 0; // blue
+          }
+        }
+        ctx.putImageData(imageData, 0, 0);
+        x += 2000;
+    } else {
+        ctx.clearRect(0, 0, clearX, clearY);
+        init(canvasSetup);
     }
+},
+canvasSetup = function(img){
+    img.onload = function() {
+        imgW          = img.width;
+        imgH          = img.height;
 
-    ctx.putImageData(imageData, 0, 0);
-    x += 7000;
-}
+        clearX        = CanvasXSize;
+        clearY        = CanvasYSize;
+
+        // get canvas context
+        canvas        = document.getElementById('image-canvas')
+        ctx           = canvas.getContext('2d');
+        canvas.width  = imgW;
+        canvas.height = imgH;
+
+        // set refresh rate
+        var drawEffect = setInterval(draw, speed);
+    }
+};
+
+init(canvasSetup);
