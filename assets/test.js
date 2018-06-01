ebc5678e3ec9f7f484ebaee24c64e46d433439ed
enylund <nylund.eric@gmail.com>
2018-06-01 10:34:32 -0400

    updates image draw animation

--- assets/test.js
+++ assets/test.js
@@ -1,5 +1,5 @@
 // Global variables
-var speed = 40,
+var speed = 60,
 x = 0,
 imgW,
 imgH,
@@ -10,7 +10,13 @@ srcArray,
 CanvasXSize,
 CanvasYSize,
 init = function (callback) {
-    var srcArray = ['img/hiphopdx/2018-05-18/nick-grant.jpg', 'img/aljazeera/2018-05-22/f1398e29dd204095a73c1829c4daf866_6.png'];
+    var srcArray = ['img/hiphopdx/2018-05-18/nick-grant.jpg',
+                    'img/aljazeera/2018-05-22/f1398e29dd204095a73c1829c4daf866_6.png',
+                    'img/infowars/2018-05-24/12-7-17-trump-mueller-610x320.jpg',
+                    'img/thesource/2018-05-22/LL-Cool-J-Inspired-to-Raise-Money-for-Cancer-Research-Following-Wifes-Diagnosis.jpg',
+                    'img/techradar/2018-05-18/4a71110f1759755d76e5095a0f135051-320-80.jpg',
+                    'img/nationalenquirer/2018-05-18/robert-kennedy-rfk-rose-love-child-f.jpg'
+                    ];
 
     img          = new Image();
     img.src      = srcArray[Math.floor(Math.random()*srcArray.length)];;
@@ -27,8 +33,8 @@ draw = function draw() {
     img.style.display = 'none';
     var imageData     = ctx.getImageData(0, 0, canvas.width, canvas.height);
     var data          = imageData.data;
-
-    if (x < data.length - 10000) {
+    console.log(data)
+    if (x < data.length) {
         for (var i = 0; i < data.length; i += 4) {
           if (i > x){
             data[i + 3] = 0;
@@ -42,6 +48,22 @@ draw = function draw() {
         }
         ctx.putImageData(imageData, 0, 0);
         x += 2000;
+    } else if ( x >= data.length && x <= data.length * 2 ) {
+        console.log(x);
+        for (var i = 0; i < data.length; i += 4) {
+          if (i + data.length < x){
+            data[i + 3] = 1;
+          } else if( data[i] > 100 && data[i + 1] > 100  & data[i + 2] > 100 ){
+            data[i + 3] = 0;
+          } else {
+            data[i]     = 0; // red
+            data[i + 1] = 0; // green
+            data[i + 2] = 0; // blue
+          }
+        }
+        ctx.putImageData(imageData, 0, 0);
+        x += 2000;
+
     } else {
         ctx.clearRect(0, 0, clearX, clearY);
         init(canvasSetup);
